720cb5676718e331f292a59f84e646408e23554a
donkorBN <projectmanuel89@gmail.com>
2024-08-26 22:04:10 +0000

    Day 1

---
+++ 001-contains_duplicate.py
@@ -0,0 +1,17 @@
+"""
+Two Sum
+https://leetcode.com/problems/contains-duplicate/description/
+
+Problem description :
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+"""
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        arr = set()
+
+        for i in nums:
+            if i in arr:
+                return True
+            arr.add(i)
+        return False
