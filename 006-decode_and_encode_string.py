1331ddcf55ee54d66d8d2ad0fa9b1d869c602e92
donkorBN <projectmanuel89@gmail.com>
2024-09-04 17:07:15 +0000

    day 7

--- 006-decode_and_encode_string.py
+++ 006-decode_and_encode_string.py
@@ -1,10 +1,31 @@
-'''
-Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+"""
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.Please implement encode and decode
 
-Please implement encode and decode
-'''
 
+"""
 class Solution:
-        def def encode(self, strs: List[str]) -> str:
 
-        def decode(self, s: str) -> List[str]:
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0 
+        
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            
+            length = int(s[i:j])
+            
+            decoded.append(s[j+1:j+1+length])
+          
+            i = j + 1 + length
+        
+        return decoded
+
+//review
