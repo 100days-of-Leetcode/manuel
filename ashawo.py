2f154240a633d5751f75ec59e449346c9a763d29
donkorBN <projectmanuel89@gmail.com>
2025-02-06 16:23:59 +0000

    dfds

---
+++ ashawo.py
@@ -0,0 +1,49 @@
+class Ashawo:
+    def __init__(self, name, age, isActive, streak):
+        self.name = name
+        self.age = age
+        self.streak = streak
+        self.isActive = isActive
+
+    def current_status(self):
+        print(f"The status of {self.name} is {self.isActive}")
+
+    def show_streak(self):
+        if self.streak <= 1:
+            print("Good")
+        elif self.streak == 2:
+            print("hmm")
+        else:
+            print("Too much chale")
+
+class Pro(Ashawo):
+    def __init__(self, name, age, isActive, streak, stats):
+        super().__init__(self.name, self.age, self.isActive,self.streak)
+        self.stats = stats
+
+    def get_current_status(self):
+        return self.stats
+
+    def set_current_status(self, status):
+        if status == "Active":
+            print("In the game")
+        else:
+            print("Retired")
+
+
+
+drew = Ashawo("Drew", 21, True, 5)
+drew.current_status()
+drew.show_streak()
+
+safo = Ashawo("Safo", 21, True, 7)
+safo.current_status()
+safo.show_streak()
+
+odk = Ashawo("Odk", 22, True, 4)
+odk.current_status()
+odk.show_streak()
+
+nana = Ashawo("Nana", 20, False, 0)
+nana.current_status()
+nana.show_streak()
---
+++ assignment.py
@@ -0,0 +1,70 @@
+"""You are required to create a CommissionEmployee class in Python to represent an employee who
+earns compensation based on a percentage of their total sales. The class must enforce encapsulation
+principles by restricting direct access to sensitive attributes."""
+
+class CommissionEmployee:
+
+    def __init__(self,first_name,last_name,social_security_number, gross_sales, commission_rate):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.social_security_number = social_security_number
+        self.__gross_sales = gross_sales
+        self.__commission_rate = commission_rate
+
+    def get_gross_sales(self):
+        return self.__gross_sales
+
+    def set_gross_sales(self, value):
+        if value <= 0.0:
+            print("Gross sales must be more than zero")
+        else:
+            self.__gross_sales = value
+
+    def get_commission_rate(self):
+        return self.__commission_rate
+
+    def set_commission_rate(self, value):
+        if 0.0 < value <= 1.0:
+            self.__commission_rate = value
+        else:
+            print("Commission rate must be between 0.0 and 1.0")
+
+    def display_employee(self):
+       print(f"{self.first_name},{self.last_name}, {self.social_security_number}, {self.__gross_sales}, {self.__commission_rate}")
+
+    def earnings(self):
+        return self.__gross_sales * self.__commission_rate
+
+class BasePlusCommissionEmployee(CommissionEmployee):
+        def __init__(self,first_name,last_name,social_security_number, gross_sales, commission_rate, _base_salary):
+            super().__init__(first_name, last_name, social_security_number,gross_sales, commission_rate)
+            self.__base_salary = _base_salary
+
+        def earnings(self):
+            #total earnings = base salary + earnings from commissions
+            return self.__base_salary + super().earnings()
+
+        def set_base_salary(self, new_salary):
+            if new_salary >= 0:
+                self.__base_salary = new_salary
+            else:
+                print("Base salary must be greater than or equal to zero")
+
+        def get_base_salary(self):
+            return self.__base_salary
+
+        def display_employee(self):
+            super().display_employee()
+            print(f"Base Salary: {self.__base_salary}")
+
+
+emp1 = CommissionEmployee("Daniel", "Akaba", 34343422, 145072.0,0.25)
+emp1.display_employee()
+print(emp1.earnings())
+
+emp2 = BasePlusCommissionEmployee ("Abena", "Joyce", 23443545, 433342, 0.24, 3000)
+emp2.display_employee()
+print(emp2.earnings())
+emp2.set_base_salary(5003)
+print(f"Updated earnings {emp2.earnings()}")
+print(f"updated name {emp2.first_name}")
