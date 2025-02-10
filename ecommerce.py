da546da5fe47cea1b185a69684ff54ab2265059d
donkorBN <projectmanuel89@gmail.com>
2025-02-10 15:59:16 +0000

    fix: corrected indentation to minimize errors

--- ecommerce.py
+++ ecommerce.py
@@ -1,47 +1,47 @@
 class Product:
     def __init__(self, product_Id, product_Name, base_Price):
-    self.product_Id = product_Id
-    self.product_Name = product_Name
-    self.base_Price = base_Price
+        self.product_Id = product_Id
+        self.product_Name = product_Name
+        self.base_Price = base_Price
 
     def apply_discount(self, percentage):
-    discount_amount = (percentage / 100) * self.base_Price
-    self.base_Price -= discount_amount
+        discount_amount = (percentage / 100) * self.base_Price
+        self.base_Price -= discount_amount
 
     def calculate_final_price(self):
-    return self.base_Price
+        return self.base_Price
 
 class Electronics(Product):
     def __init__(self, product_Id, product_Name, base_Price,
     warranty_period):
-    super().__init__(product_Id, product_Name, base_Price)
-    self.warranty_period = warranty_period
+        super().__init__(product_Id, product_Name, base_Price)
+        self.warranty_period = warranty_period
 
     def calculate_final_price(self):
-    warranty_based_price = self.base_Price + (self.base_Price * 0.05 *
-    self.warranty_period / 12)
-    return warranty_based_price
+        warranty_based_price = self.base_Price + (self.base_Price * 0.05 *
+        self.warranty_period / 12)
+        return warranty_based_price
 
 class Clothing(Product):
     def __init__(self, product_Id, product_Name, base_Price, size,
     fabric_charge):
-    super().__init__(product_Id, product_Name, base_Price)
-    self.size = size
-    self.fabric_charge = fabric_charge
+        super().__init__(product_Id, product_Name, base_Price)
+        self.size = size
+        self.fabric_charge = fabric_charge
 
     def calculate_final_price(self):
-    return self.base_Price + self.fabric_charge
+        return self.base_Price + self.fabric_charge
 
 class Cart:
     def __init__(self):
-    self.products = []
-    def add_product(self, product):
-    self.products.append(product)
+        self.products = []
+        def add_product(self, product):
+        self.products.append(product)
 
     def calculate_total_price(self):
-    total_price = sum(product.calculate_final_price() for product in
-    self.products)
-    return total_price
+        total_price = sum(product.calculate_final_price() for product in
+        self.products)
+        return total_price
 
 laptop = Electronics("E001", "Laptop", 1000.0, 24)
 jacket = Clothing("C001", "Winter Jacket", 200.0, "M", 20.0)
