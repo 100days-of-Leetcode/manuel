1e9b025dcb0e166aca4aea5bd27d183a052547e7
donkorBN <projectmanuel89@gmail.com>
2025-02-18 13:49:29 +0000

    dsd

--- assignment.py
+++ assignment.py
@@ -60,6 +60,7 @@ class BasePlusCommissionEmployee(CommissionEmployee):
 
 emp1 = CommissionEmployee("Daniel", "Akaba", 34343422, 145072.0,0.25)
 emp1.display_employee()
+emp1.
 print(emp1.earnings())
 
 emp2 = BasePlusCommissionEmployee ("Abena", "Joyce", 23443545, 433342, 0.24, 3000)
